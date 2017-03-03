4375ed04e2176dc3ac78505a7186ef2f9a772491
VitaZubkova <vitazubkova@yandex.ru>
2017-03-03 13:45:37 +0300

    la-la-la(test)

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,24 @@
-var ball = document.querySelector(".discoball");
+var ball = document.querySelector('.discoball');
+var classes = ['here','goes','our','awesome','disco','ball'];
+var i=0;
 
-function goDisco() {
+function goDisco(){
 	//do your disco magic here !
+	
+ball.setAttribute('class','discoball ' + classes[i]);
+
+i++;
+	if(i>5){
+			i=0;
+		
+	}
+	
 }
 
+
+
+
 setInterval(goDisco,300);
+
+
+
